c5fc962b61995ff58e19d0709bb72708dcd75421
范舒阳 <fanshuyang@bytedance.com>
2023-04-07 02:50:19 +0800

    doc: update example

--- examples/main.go
+++ examples/main.go
@@ -15,7 +15,7 @@ func main() {
 	h := server.Default()
 
 	h.GET("/curl", func(ctx context.Context, c *app.RequestContext) {
-		cmd := curl.GetCurlCommand(&c.Request)
+		cmd, _ := curl.GetCurlCommand(&c.Request)
 		hlog.Info(cmd.String())
 		// 2023/04/06 22:33:00.044947 main.go:15: [Info] curl -X 'GET' -H 'Accept: */*' -H 'Accept-Encoding: gzip, deflate, br' -H 'Connection: keep-alive' -H 'Host: localhost:8888' -H 'Postman-Token: bc98e52c-e9fd-4c71-895b-9a27d940f151' -H 'User-Agent: PostmanRuntime/7.29.2' 'http://localhost:8888/curl'
 		c.JSON(consts.StatusOK, utils.H{"curl": cmd.String()})
